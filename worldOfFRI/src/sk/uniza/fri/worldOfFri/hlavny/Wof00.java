c56665e8bb58a8f3839a98a5d032791ffdd084d4
Jan Janech <jan.janech@kst.uniza.sk>
2020-02-25 11:25:39 +0100

    02b - zadanie uloh

--- worldOfFRI/src/sk/uniza/fri/worldOfFri/hlavny/Wof00.java
+++ worldOfFRI/src/sk/uniza/fri/worldOfFri/hlavny/Wof00.java
@@ -1,5 +1,46 @@
 package sk.uniza.fri.worldOfFri.hlavny;
 
+/*
+Ulohy - questy
+- objav novu miestnost (zatajena - nie je v zozname vychodov)
+- prejdi celu mapu
+- zabi nepriatela
+Npc - non-playable character
+- dekan
+- bufetarka
+- vratnicka
+- skolnik
+- upratovacka - prisera
+- velke serverove monstrum
+- akcie:
+  - dava questy
+  - obchod
+  - rozhovor
+  - suboj
+Predmety
+- mec
+- bageta, dodava staminu
+- oravska mineralka, zvysuje agilitu
+- peniaze
+- isic - otvaranie miestnosti
+- notebook - dekodovanie zaheslovanych vstupov
+- akcie:
+  - pouzitie
+  - uschovanie
+  - zahodenie
+Inventar
+Spelly
+- dekodovanie pomocou notebooku
+- alt enter
+- meditacia - prida staminu za nejaky cas
+Atributy
+- stamina - ked bude dekodovat zobere 3 z 10
+- agilita - rychlost pohybu
+- inteligencia
+- hp
+- charizma, vystupenie zo suboja
+*/
+
 /**
  * Hlavna trieda hry WoF s metodou main - spustanie v NB
  * 
