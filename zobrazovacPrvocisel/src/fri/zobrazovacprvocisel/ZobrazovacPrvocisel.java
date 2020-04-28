a3b753f43f4d3baaad3fadd97b0c923f598310bf
Jan Janech <jan.janech@kst.uniza.sk>
2020-04-28 11:07:15 +0200

    10b - projekt pre zobrazovac prvocisel

---
+++ zobrazovacPrvocisel/src/fri/zobrazovacprvocisel/ZobrazovacPrvocisel.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fri.zobrazovacprvocisel;
+
+/**
+ *
+ * @author janik
+ */
+public class ZobrazovacPrvocisel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Prvocisla p = new Prvocisla(10, 100);
+        /*for (Integer prvocislo : p) {
+            System.out.println(prvocislo);
+        }*/
+    }
+    
+}
