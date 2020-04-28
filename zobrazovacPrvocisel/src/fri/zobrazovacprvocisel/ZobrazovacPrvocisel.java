a5ac95136b91d61efe9fab9bf8b387988eabaf5e
Jan Janech <jan.janech@kst.uniza.sk>
2020-04-28 11:12:42 +0200

    10b - rozpisany cyklus foreach

--- zobrazovacPrvocisel/src/fri/zobrazovacprvocisel/ZobrazovacPrvocisel.java
+++ zobrazovacPrvocisel/src/fri/zobrazovacprvocisel/ZobrazovacPrvocisel.java
@@ -5,6 +5,8 @@
  */
 package fri.zobrazovacprvocisel;
 
+import java.util.Iterator;
+
 /**
  *
  * @author janik
@@ -17,6 +19,13 @@ public class ZobrazovacPrvocisel {
     public static void main(String[] args) {
         // TODO code application logic here
         Prvocisla p = new Prvocisla(10, 100);
+        
+        /*Iterator<Integer> it = p.iterator();
+        while (it.hasNext()) {
+            Integer prvocislo = it.next();
+            System.out.println(prvocislo);
+        }*/
+        
         /*for (Integer prvocislo : p) {
             System.out.println(prvocislo);
         }*/
