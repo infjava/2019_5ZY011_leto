a9d11db7728be24b2ace10c63e6176468c031be7
Jan Janech <jan.janech@kst.uniza.sk>
2020-05-04 16:37:02 +0200

    11a - kombinacia borderlayoutu a gridlayoutu

--- vtipnaApp/src/fri/vtipnaapp/VtipneOkno.java
+++ vtipnaApp/src/fri/vtipnaapp/VtipneOkno.java
@@ -6,9 +6,11 @@
 package fri.vtipnaapp;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.WindowConstants;
 
 /**
@@ -22,10 +24,13 @@ class VtipneOkno {
     public VtipneOkno() {
         this.okno = new JFrame("Otázka");
         this.okno.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        this.okno.setLayout(new BorderLayout());
+        this.okno.setLayout(new BorderLayout(10, 10));
         this.okno.add(new JLabel("Chceš dostať A zo skúšky?"), BorderLayout.NORTH);
-        this.okno.add(new JButton("áno"), BorderLayout.WEST);
-        this.okno.add(new JButton("nie"), BorderLayout.CENTER);
+        JPanel tlacidla = new JPanel();
+        tlacidla.setLayout(new GridLayout());
+        tlacidla.add(new JButton("áno"));
+        tlacidla.add(new JButton("nie"));
+        this.okno.add(tlacidla, BorderLayout.CENTER);
         this.okno.pack();
     }
     
