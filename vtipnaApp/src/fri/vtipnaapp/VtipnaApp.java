6398da8f3ad8893834d3309160aa82a1d0616921
Jan Janech <jan.janech@kst.uniza.sk>
2020-05-04 17:36:42 +0200

    11a - systemova tema

--- vtipnaApp/src/fri/vtipnaapp/VtipnaApp.java
+++ vtipnaApp/src/fri/vtipnaapp/VtipnaApp.java
@@ -1,8 +1,16 @@
 package fri.vtipnaapp;
 
+import javax.swing.UIManager;
+
 public class VtipnaApp {
 
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        
         VtipneOkno okno = new VtipneOkno();
         okno.zobraz();
     }
